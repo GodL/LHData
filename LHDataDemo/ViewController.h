f1757c25acf679abd79c1a5fb3f6d52191d0a062
godL <547188371@qq.com>
2016-04-12 15:24:58 +0800

    Initial Commit

---
+++ LHDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHDataDemo
+//
+//  Created by 3wchina01 on 16/4/12.
+//  Copyright © 2016年 3wchina01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LHDataDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LHDataDemo
+//
+//  Created by 3wchina01 on 16/4/12.
+//  Copyright © 2016年 3wchina01. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
